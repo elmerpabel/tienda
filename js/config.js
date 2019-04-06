9aa34baaa42d8c4b42dcb17ebe99f6aba81da9a1
elmerpabel <48300603+elmerpabel@users.noreply.github.com>
2019-04-06 09:38:02 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -73,7 +73,7 @@ $(function() {
 	simpleStore.init({
 
 		// brand can be text or image URL
-		brand : "SimpleStore",
+		brand : "Simple Tienda",
 
 		// numder of products per row (accepts 1, 2 or 3)
 		numColumns : 3,
